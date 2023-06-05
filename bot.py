de1719e296ef64aef6051ae6d1504271d09e9691
Artem Striver <dog-di@mail.ru>
2023-06-05 21:56:15 +0500

    The first version of the bot with the echo function.

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+import random
 from _token import token
 import vk_api
 import vk_api.bot_longpoll
+from vk_api.utils import get_random_id
+
 group_id = 220983890
 
 
@@ -14,8 +17,27 @@ class Bot:
         self.vk = vk_api.VkApi(token=self.token)
         self.long_poller = vk_api.bot_longpoll.VkBotLongPoll(vk=self.vk, group_id=group_id)
 
+        self.api = self.vk.get_api()
+
     def run(self):
-        pass
+        for event in self.long_poller.listen():
+            print('получено событие')
+            try:
+                self.on_event(event)
+            except Exception as exc:
+                print(exc)
+
+    def on_event(self, event):
+        if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
+            print(event.message.text)
+            print(event.message.peer_id)
+            self.api.messages.send(
+                message=event.message.text,
+                random_id=random.randint(0, 2**20),
+                peer_id=event.message.peer_id,
+            )
+        else:
+            print('такое событие обработать пока не  могу - ', event.type)
 
 
 if __name__ == '__main__':
