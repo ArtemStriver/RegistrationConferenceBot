25863a7676253f0e5594d5a4798110fe87ec3e2d
Artem Striver <dog-di@mail.ru>
2023-06-06 08:36:19 +0500

    The first version of the bot with the echo function. I clean my code.

--- bot.py
+++ bot.py
@@ -13,15 +13,12 @@ class Bot:
     def __init__(self, group_id, token):
         self.group_id = group_id
         self.token = token
-
         self.vk = vk_api.VkApi(token=self.token)
         self.long_poller = vk_api.bot_longpoll.VkBotLongPoll(vk=self.vk, group_id=group_id)
-
         self.api = self.vk.get_api()
 
     def run(self):
         for event in self.long_poller.listen():
-            print('получено событие')
             try:
                 self.on_event(event)
             except Exception as exc:
@@ -29,11 +26,9 @@ class Bot:
 
     def on_event(self, event):
         if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
-            print(event.message.text)
-            print(event.message.peer_id)
             self.api.messages.send(
                 message=event.message.text,
-                random_id=random.randint(0, 2**20),
+                random_id=random.randint(0, 2 ** 20),
                 peer_id=event.message.peer_id,
             )
         else:
