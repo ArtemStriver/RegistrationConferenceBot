57fcdf664e5ebcaa5a935f12b33c9fc2f9ce2bda
Artem Striver <dog-di@mail.ru>
2023-06-13 12:40:03 +0500

    The second version of the bot with the echo function. I added logging.

--- bot.py
+++ bot.py
@@ -2,11 +2,25 @@
 
 import random
 from _token import token
+import logging
 import vk_api
-import vk_api.bot_longpoll
-from vk_api.utils import get_random_id
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 group_id = 220983890
+log = logging.getLogger('bot')
+
+
+def configure_logging():
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(logging.Formatter('%(levelname)s - %(message)s'))
+    stream_handler.setLevel(logging.INFO)
+    log.addHandler(stream_handler)
+
+    file_handler = logging.FileHandler(filename='bot.log', encoding='UTF-8')
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M'))
+    file_handler.setLevel(logging.DEBUG)
+    log.addHandler(file_handler)
+    log.setLevel(logging.DEBUG)
 
 
 class Bot:
@@ -14,27 +28,29 @@ class Bot:
         self.group_id = group_id
         self.token = token
         self.vk = vk_api.VkApi(token=self.token)
-        self.long_poller = vk_api.bot_longpoll.VkBotLongPoll(vk=self.vk, group_id=group_id)
+        self.long_poller = VkBotLongPoll(vk=self.vk, group_id=group_id)
         self.api = self.vk.get_api()
 
     def run(self):
         for event in self.long_poller.listen():
             try:
                 self.on_event(event)
-            except Exception as exc:
-                print(exc)
+            except Exception:
+                log.exception('ошибка в обработке события')
 
     def on_event(self, event):
-        if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            log.debug('отправляем сообщение назад')
             self.api.messages.send(
                 message=event.message.text,
                 random_id=random.randint(0, 2 ** 20),
                 peer_id=event.message.peer_id,
             )
         else:
-            print('такое событие обработать пока не  могу - ', event.type)
+            log.info('такое событие обработать пока не  могу - %s', event.type)
 
 
 if __name__ == '__main__':
+    configure_logging()
     bot = Bot(group_id=group_id, token=token)
     bot.run()
